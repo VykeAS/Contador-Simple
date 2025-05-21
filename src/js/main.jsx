75905a59e73185ccf4adea563fc3d3ade376f51e
Enrique <enrique2002.s.m@gmail.com>
2025-05-21 15:46:29 +0200

    .

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -2,18 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
-// index.css
 import '../styles/index.css';
 
-// Components
 import Counter from './components/Contador';
 
 let sumatoria = 0;
-
 
 function sumarSegundos() {
   
