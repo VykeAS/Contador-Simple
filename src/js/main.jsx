6a22cdcc22ca9ff72a84443abcf42748514419fc
Enrique <enrique2002.s.m@gmail.com>
2025-05-21 15:47:22 +0200

    .

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,12 +1,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
-
 import '../styles/index.css';
-
 import Counter from './components/Contador';
 
 let sumatoria = 0;
